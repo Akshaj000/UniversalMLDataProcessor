bf3ae70220ca6bb1d886f6eeefd16e50818423e9
Abhinavxox <abhinavxox@gmail.com>
2023-01-13 15:34:38 +0530

    <cm>:[]

--- client/js/process.js
+++ client/js/process.js
@@ -1,4 +1,4 @@
-document.getElementById("start").addEventListener("click", () => {
+document.getElementById("start").addEventListener("click", async () => {
   var typeChosen = document.querySelector(
     'input[name="question1"]:checked'
   ).value;
@@ -29,22 +29,181 @@ document.getElementById("start").addEventListener("click", () => {
     can_apply_smote: SmoteChosen == "true" ? true : false,
     scaler: ScalerChosen,
   };
-  process(data)
+  responseData = await process(data);
+
+  //graphs and output:
+  if (typeChosen == "Regression") {
+    MAE(responseData);
+    MAPE(responseData);
+    MSE(responseData);
+    r2score(responseData);
+  }
 });
 
-const process = async(data)=>{
-    await fetch("http://localhost:5000/process", {
+//fucntion to fetch
+const process = async (data) => {
+  await fetch("http://localhost:5000/process", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-
     },
-    mode : 'cors',
+    mode: "cors",
     body: JSON.stringify(data),
   })
-  .then(res => res.json())
-  .then(response => {
-      console.log(JSON.stringify(response))
-  })
-  .catch(error => console.error('Error:', error));
-}
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(JSON.stringify(response));
+      responseData = response;
+    })
+    .catch((error) => console.error("Error:", error));
+
+  return responseData;
+};
+
+/*
+  for graphs and output
+*/
+
+//to print MAE
+const MAE = (responseData) => {
+  // Extract the MAE
+  var labels = responseData && Object.keys(responseData);
+
+  var MAE = labels.map(function (label) {
+    return responseData[label]["MAE"];
+  });
+  // Get the canvas element
+  var ctx = document.getElementById("MAE").getContext("2d");
+
+  // Create a new chart
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "MAE",
+          data: MAE,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
+//to print MAPE
+const MAPE = (responseData) => {
+  // Extract the MAPE
+  var labels = responseData && Object.keys(responseData);
+
+  var MAPE = labels.map(function (label) {
+    return responseData[label]["MAPE"];
+  });
+  // Get the canvas element
+  var ctx = document.getElementById("MAPE").getContext("2d");
+
+  // Create a new chart
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "MAPE",
+          data: MAPE,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
+//to print MSE
+const MSE = (responseData) => {
+  // Extract the MSE
+  var labels = responseData && Object.keys(responseData);
+
+  var MSE = labels.map(function (label) {
+    return responseData[label]["MSE"];
+  });
+  // Get the canvas element
+  var ctx = document.getElementById("MSE").getContext("2d");
+
+  // Create a new chart
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "MSE",
+          data: MSE,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
+//to print r2score
+const r2score = (responseData) => {
+  // Extract the r2 scores
+  var labels = responseData && Object.keys(responseData);
+
+  var r2Scores = labels.map(function (label) {
+    return responseData[label]["r2 score"];
+  });
+  // Get the canvas element
+  var ctx = document.getElementById("R2Score").getContext("2d");
+
+  // Create a new chart
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "R2 Score",
+          data: r2Scores,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
